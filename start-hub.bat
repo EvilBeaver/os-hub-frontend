b4b746679930765fea89f95f6d9890a93d80bcdc
Tymko Oleg <olegtymko@yandex.ru>
2019-06-02 14:43:09 +0700

    Наброски

---
+++ start-hub.bat
@@ -0,0 +1,6 @@
+cd D:/DATA/Develop/Project/os-hub-frontend/src
+SET OSHUB_BINARY_ROOT
+SET GITHUB_SUPER_TOKEN
+SET ASPNETCORE_URLS=http://0.0.0.0:80
+SET OSHUB_IMPORT
+D:/DATA/Develop/Project/oscript-site/oscriptweb/OneScript.WebHost.exe
